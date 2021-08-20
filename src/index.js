539ca331f156b90caa62ec8b45bcdb9a5b1502a9
azkmee <azmi_aang@hotmail.com>
2021-08-20 10:20:31 +0800

    tailwind build

--- src/index.js
+++ src/index.js
@@ -1,5 +1,6 @@
 import * as React from "react";
 import * as ReactDOM from "react-dom";
 import { Marketplace } from "./pages/marketplace";
+import './index.css';
 
 ReactDOM.render(<Marketplace />, document.querySelector("#root"));
